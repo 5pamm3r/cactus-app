31cb682e214894e37419dc208695be06cd6101ed
5pamm3r <cmassarino.dev@gmail.com>
2022-05-07 15:07:43 -0300

    remove basura

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <header />
+        <div className='search__container'>
+          <input type='text' placeholder='Search...' />
+        </div>
+        <nav className='nav__container'>
+          <ul>
+            <li>All</li>
+            <li>Indoor</li>
+            <li>Outdoor</li>
+            <li>Favorite</li>
+          </ul>
+        </nav>
+      </React.Fragment>
+      
   );
 }
 
---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header>
+      <p>Discovery</p>
+      <img src='' alt='perfil icon'/>
+    </header>
+  )
+}
+
+export { Header }
